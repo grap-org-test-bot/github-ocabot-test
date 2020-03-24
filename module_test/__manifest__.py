a5f9aba33fe814dc17f6014e396e31fd9ba62697
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-03-24 20:24:53 +0100

    V8

--- module_test/__manifest__.py
+++ module_test/__manifest__.py
@@ -3,10 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Module Test 7",
+    "name": "Module Test 8",
     "version": "12.0.1.1.0",
     "category": "Demo",
-    "summary": "Module Test for ocabot 7",
+    "summary": "Module Test for ocabot 8",
     "maintainers": ["legalsylvain", "quentinDupont"],
     "author": "GRAP",
     "website": "http://www.grap.coop",
